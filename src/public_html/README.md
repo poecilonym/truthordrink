bd11167e5c41245b766ca9924bf8495719ca8ecc
oskar18j <oskar@zeichenkraft.de>
2022-01-28 19:00:40 +0100

    New design

---
+++ src/public_html/README.md
@@ -0,0 +1,9 @@
+# truthordrink
+
+this is a small truth or dare game that is intended for parties and the such
+
+## todo
+
+- write general lists of truths and dares
+- implement users adding to the lists or creating their own
+- implement more modes
---
+++ src/public_html/src/main.js
@@ -0,0 +1,30 @@
+let foo = {};
+let truths = [];
+let dares = [];
+let baa = $.getJSON("data.json")
+
+baa.done(data => { });
+
+baa.then(data => {
+    console.log(data.truth);
+    for (let i = 0; i < data.truth.length; i++) {
+        truths.push(data.truth[i]);
+    }
+    for (let i = 0; i < data.dare.length; i++) {
+        dares.push(data.dare[i]);
+    }
+});
+
+console.log(truths);
+console.log(dares);
+let index = 0;
+const truth = () => {
+    index = Math.floor(Math.random() * truths.length);
+    console.log(index);
+    document.getElementById("answer").innerHTML = truths[index];
+}
+const dare = () => {
+    index = Math.floor(Math.random() * dares.length);
+    console.log(index);
+    document.getElementById("answer").innerHTML = dares[index];
+}
