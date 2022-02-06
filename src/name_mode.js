c717bfaf2e6af41b9e8c1777099a88933fab769f
poecilonym <jrsaure10@gmail.com>
2022-02-06 22:06:43 +0100

    further features for the third gamemode, not fully working yet

--- src/name_mode.js
+++ src/name_mode.js
@@ -2,23 +2,36 @@ let names = [];
 const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
-let list = urlParams.get('wow');
+let list = urlParams.get('names');
 console.log(list);
 list = $.parseJSON(list);
 console.log(list);
 
 const naming = urlParams.get('naming');
 
-if(naming == "true") {
-    console.log("naming mode");
-    let buttonelement = document.getElementById("truth");
-}
-else {
-    document.getElementById("addnames").innerHTML = " ";
+const spec_name = () => {
+    if(naming == "true") {
+        console.log("naming mode");
+        let buttonelement = document.getElementById("button_output");
+        buttonelement.remove();
+    }
+    else {
+        document.getElementById("addnames").remove();
+    }
 }
 
+let name = "";
+let lastname = "";
+
 const rand_name = () => {
-    document.getElementById("name").innerHTML = names[Math.floor(Math.random() * names.length)];
+    lastname = name
+    while(true) {
+        name = list[Math.floor(Math.random() * list.length)];
+        if(name != lastname || list.length <= 1) {
+            break;
+        }
+    }
+    document.getElementById("name").innerHTML = name; 
     truth_or_dare = Math.floor(Math.random() * 2);
     if (truth_or_dare == 0) {
         document.getElementById("req_name").innerHTML = "Truth:"
@@ -28,4 +41,24 @@ const rand_name = () => {
         document.getElementById("req_name").innerHTML = "Dare:"
         dare();
     }
+}
+
+const add_name = () => {
+    let inpt_name = n;
+}
+    
+const start_game =() => {
+    if(list.length == 0) {
+        document.getElementById("err").innerHTML = "Please enter at least one name";
+        return;
+    }
+    let link = "name_mode.html";
+    link = link.concat("?", "naming", "=", "false", "&names=[\"", list[0], "\"");
+    for(let i = 1; i < list.length; i++) {
+        link = link.concat(",\"", list[i], "\"");
+    }
+    link = link.concat("]");
+    console.log(link);
+    window.location.href = link;
+    //window.location.replace(link);
 }
